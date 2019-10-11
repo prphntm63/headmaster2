cd16b94e5f97544226f9021f5d8b9739049afa60
Matt Westwick <matt.westwick@gmail.com>
2019-10-11 01:14:21 -0500

    trying to get github not to be a URL

--- seeds/004_create_students.js
+++ seeds/004_create_students.js
@@ -123,7 +123,6 @@ function randomDate(start, end, startHour, endHour) {
   var hour = startHour + Math.random() * (endHour - startHour) | 0;
   date.setHours(hour);
   let dateString = date.toISOString()
-  console.log(dateString)
   return dateString
 }
 
