8db9a6400015322c70ddfb987d910b06ed180252
Matt Westwick <matt.westwick@gmail.com>
2020-01-22 19:13:07 -0600

    Updates

---
+++ precache-manifest.86c87cea5496fb925ce46ef2d2044930.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04eeab3df0a2cb379917c4f65d7e8962",
+    "url": "/headmaster2/index.html"
+  },
+  {
+    "revision": "4dac0f3fdd4d0a2e9e9c",
+    "url": "/headmaster2/static/css/main.f377ba2a.chunk.css"
+  },
+  {
+    "revision": "c53644cae0b126f9a967",
+    "url": "/headmaster2/static/js/2.8a8bc70b.chunk.js"
+  },
+  {
+    "revision": "4dac0f3fdd4d0a2e9e9c",
+    "url": "/headmaster2/static/js/main.0e55ca58.chunk.js"
+  },
+  {
+    "revision": "304dc6ee4c930325e8eb",
+    "url": "/headmaster2/static/js/runtime-main.158cdb07.js"
+  }
+]);
